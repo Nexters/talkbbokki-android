6465d00cfb868d2fbb9025a519260501cfa1cc15
jiyoung <ji9310@naver.com>
2023-01-31 19:51:40 +0900

    #5 ui-sample : card 인터렉션 샘플 화면 생성

---
+++ app/src/main/java/com/hammer/talkbbokki/CardSampleActivity.kt
@@ -0,0 +1,11 @@
+package com.hammer.talkbbokki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_sample)
+    }
+}
