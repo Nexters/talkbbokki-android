055772877c1f763f71784a15f03a140a2ce27601
jiyoung <ji9310@naver.com>
2023-02-01 20:06:58 +0900

    rootProject명 바꾸기
    - talkbbokki-android 로 명칭 변경

--- app/src/main/java/com/hammer/talkbbokki/ui/theme/Theme.kt
+++ app/src/main/java/com/hammer/talkbbokki/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun IcebreakerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun TalkbbokkiTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
