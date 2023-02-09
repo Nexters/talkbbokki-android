2adc8cffe18f6ee9bc4ceee3920c6f97f0375e09
seunghee63 <0603yang@naver.com>
2023-02-09 19:15:27 +0900

    feat: ktlintFormat 반영

--- app/src/main/java/com/hammer/talkbbokki/MainActivity.kt
+++ app/src/main/java/com/hammer/talkbbokki/MainActivity.kt
@@ -8,10 +8,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hammer.talkbbokki.ui.theme.IcebreakerTheme
-import androidx.compose.ui.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
