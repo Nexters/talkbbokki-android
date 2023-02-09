d56857c19d9bf8b0a090b4a5b3830923691398b8
jiyoung <ji9310@naver.com>
2023-02-10 02:08:00 +0900

    #9 : 메인화면 레벨 데이터 넣기

--- app/src/main/java/com/hammer/talkbbokki/presentation/main/MainScreen.kt
+++ app/src/main/java/com/hammer/talkbbokki/presentation/main/MainScreen.kt
@@ -1,6 +1,10 @@
 package com.hammer.talkbbokki.presentation.main
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -9,9 +13,24 @@ fun MainRoute(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = hiltViewModel()
 ) {
-    MainScreen()
+    val categoryLevel by viewModel.categoryLevel.collectAsState()
+    MainScreen(categoryLevel)
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(categoryLevel: List<CategoryLevel>) {
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        MainHeader()
+        CategoryLevels()
+    }
+}
+
+@Composable
+fun MainHeader() {
+}
+
+@Composable
+fun CategoryLevels() {
 }
--- app/src/main/java/com/hammer/talkbbokki/ui/theme/Color.kt
+++ app/src/main/java/com/hammer/talkbbokki/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Level1BackgroundColor = Color(0xFF929292)
+val Level2BackgroundColor = Color(0xFF727272)
+val Level3BackgroundColor = Color(0xFF5B5B5B)
+val Level4BackgroundColor = Color(0xFF424242)
