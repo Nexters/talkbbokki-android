635b68788da89e2b7987a453ce4030183b4cc700
seunghee63 <0603yang@naver.com>
2023-02-11 18:07:28 +0900

    feat: 카드 선택시 scale up 및 클릭시 flip 에니메이션 (#13)

--- app/src/main/java/com/hammer/talkbbokki/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/hammer/talkbbokki/presentation/detail/DetailScreen.kt
@@ -1,11 +1,20 @@
 package com.hammer.talkbbokki.presentation.detail
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun DetailRoute(
@@ -17,13 +26,80 @@ fun DetailRoute(
 
 @Composable
 fun DetailScreen(modifier: Modifier = Modifier) {
-    // 카드 플립 애니메이션
-    BoxWithConstraints {
-        Box(
-            modifier = modifier
-                .align(Alignment.Center)
-        ) {
-            Text(text = "화면전환")
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize()
+            .size(192.dp, 240.dp)
+    ) {
+        Test()
+    }
+
+}
+
+@Composable
+fun Test() {
+    var cardFace by remember { mutableStateOf(CardFace.Front) }
+    var scale by remember { mutableStateOf(1f) }
+    var rotationY by remember { mutableStateOf(0f) }
+
+    FlipCard(
+        cardFace = cardFace,
+        onClick = { cardFace = cardFace.next },
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(.7f)
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationY = rotationY,
+                cameraDistance = 12f
+            ),
+        front = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Blue),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = "카드",
+                )
+            }
+        },
+        back = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Blue),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = "대화주제",
+                )
+            }
+        },
+    )
+
+    LaunchedEffect(key1 = Unit) {
+
+        val transformationAnimationSpec = tween<Float>(
+            durationMillis = 1000,
+            easing = FastOutSlowInEasing
+        )
+
+        coroutineScope {
+            // 카드 더 커지기
+            launch {
+                animate(
+                    initialValue = 1f, targetValue = 1.7f,
+                    animationSpec = transformationAnimationSpec
+                ) { value: Float, _: Float ->
+                    scale = value
+                }
+            }
         }
+
+        // 카드 뒤집기
     }
 }
--- app/src/main/java/com/hammer/talkbbokki/presentation/detail/FlipCard.kt
+++ app/src/main/java/com/hammer/talkbbokki/presentation/detail/FlipCard.kt
@@ -1,4 +1,61 @@
 package com.hammer.talkbbokki.presentation.detail
 
-class FlipCard {
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+
+enum class CardFace(val angle: Float) {
+    Front(0f) {
+        override val next: CardFace
+            get() = Back
+    },
+    Back(180f) {
+        override val next: CardFace
+            get() = Front
+    };
+
+    abstract val next: CardFace
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlipCard(
+    cardFace: CardFace,
+    onClick: (CardFace) -> Unit,
+    modifier: Modifier,
+    back: @Composable () -> Unit = {},
+    front: @Composable () -> Unit = {},
+) {
+    val rotation = animateFloatAsState(
+        targetValue = cardFace.angle,
+        animationSpec = tween(
+            durationMillis = 540,
+            easing = FastOutSlowInEasing,
+        )
+    )
+
+    Card(
+        onClick = { onClick(cardFace) },
+        modifier = modifier
+            .graphicsLayer {
+                rotationY = rotation.value
+            },
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            if (rotation.value <= 90f) {
+                front()
+            } else {
+                back()
+            }
+        }
+    }
 }
