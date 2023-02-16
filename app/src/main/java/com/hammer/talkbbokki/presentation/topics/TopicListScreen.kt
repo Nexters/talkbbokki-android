e1602f10e09479932f0fb4f844127d4fdd948cad
seunghee63 <0603yang@naver.com>
2023-02-17 02:10:01 +0900

    feat: 토픽 리스트 구조 정리 및 디테일 UI 반영

--- app/src/main/java/com/hammer/talkbbokki/presentation/topics/TopicListScreen.kt
+++ app/src/main/java/com/hammer/talkbbokki/presentation/topics/TopicListScreen.kt
@@ -1,5 +1,6 @@
 package com.hammer.talkbbokki.presentation.topics
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -19,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -42,11 +45,61 @@ fun TopicListRoute(
     TopicListScreen(onClickToDetail = onClickToDetail)
 }
 
+val level = "Level1"
+
+@Composable
+fun TopicListScreen(onClickToDetail: (id: String) -> Unit) {
+    var selectedIdx by remember { mutableStateOf("0") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(CategoryLevel.valueOf(level).backgroundColor)
+    ) {
+        TopicListHeader()
+        TopicList(onFocusedCardChange = { idx -> selectedIdx = idx })
+        SelectBtn(
+            onCardClicked = { onClickToDetail(selectedIdx) },
+            Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun TopicListHeader() {
+    Column(
+        modifier = Modifier
+            .padding(
+                vertical = dimensionResource(id = R.dimen.vertical_padding),
+                horizontal = dimensionResource(id = R.dimen.horizontal_padding)
+            )
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_close),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.End)
+                .size(24.dp, 24.dp),
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        stringResource(id = CategoryLevel.valueOf(level).title).split("\n")
+            .forEachIndexed { index, s ->
+                Text(
+                    text = s,
+                    style = TalkbbokkiTypography.h2_bold,
+                    color = if (index == 0) Color.Black else Color(0x80000000)
+                )
+            }
+    }
+}
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun TopicListScreen(modifier: Modifier = Modifier, onClickToDetail: (id: String) -> Unit) {
+fun TopicList(onFocusedCardChange: (idx: String) -> Unit) {
     val listState = rememberLazyListState()
-    val itemWidth = with(LocalDensity.current) { 120.dp.toPx() }
+    val itemWidth =
+        with(LocalDensity.current) { (dimensionResource(id = R.dimen.card_width)).toPx() }
     val currentIndex = remember { derivedStateOf { listState.firstVisibleItemIndex } }
     val offset = remember { derivedStateOf { listState.firstVisibleItemScrollOffset } }
 
@@ -57,132 +110,121 @@ fun TopicListScreen(modifier: Modifier = Modifier, onClickToDetail: (id: String)
 
     val cardList = (0..10).toList()
 
-    val level = "Level1"
-
-    BoxWithConstraints(
-        modifier = Modifier.background(CategoryLevel.valueOf(level).backgroundColor)
-    ) {
-        var index = ""
-
-        Column() {
-            stringResource(id = CategoryLevel.valueOf(level).title).split("\n")
-                .forEachIndexed { index, s ->
-                    Text(
-                        text = s,
-                        style = TalkbbokkiTypography.h2_bold,
-                        color = if (index == 0) Color.Black else Color(0x80000000)
-                    )
+    Column() {
+        Spacer(modifier = Modifier.weight(1f))
+        BoxWithConstraints(
+            modifier = Modifier.weight(9f)
+        ) {
+            // 대화 주제 리스트
+            LazyRow(
+                state = listState,
+                modifier = Modifier.fillMaxSize(),
+                flingBehavior = snapFlingBehavior,
+                verticalAlignment = Alignment.CenterVertically,
+                contentPadding = PaddingValues(
+                    start = ((maxWidth - (dimensionResource(id = R.dimen.card_width))) / 2),
+                    end = ((maxWidth - (dimensionResource(id = R.dimen.card_width))) / 2)
+                )
+            ) {
+                itemsIndexed(cardList) { definiteIndex, _ ->
+                    onFocusedCardChange(currentOffset.roundToInt().toString())
+                    CardItem(definiteIndex, currentOffset)
                 }
-        }
-
-        // 대화 주제 리스트
-        LazyRow(
-            state = listState,
-            modifier = modifier.fillMaxSize(),
-            flingBehavior = snapFlingBehavior,
-            verticalAlignment = Alignment.CenterVertically,
-            contentPadding = PaddingValues(
-                start = ((maxWidth - 120.dp) / 2),
-                end = ((maxWidth - 120.dp) / 2)
-            )
-        ) {
-            itemsIndexed(cardList) { definiteIndex, item ->
-                index = currentOffset.roundToInt().toString()
-                CardItems(definiteIndex, currentOffset)
             }
-        }
-        Button(
-            onClick = {
-                onClickToDetail(index)
-            },
-            modifier = modifier
-                .size(335.dp, 60.dp)
-                .background(Color.Black)
-                .align(Alignment.BottomCenter),
-            shape = RoundedCornerShape(8)
-        ) {
-            Text(text = "이 카드 뽑기")
         }
     }
 }
 
 @Composable
-fun CardItems(definiteIndex: Int, currentOffset: Float) {
+fun SelectBtn(onCardClicked: () -> Unit, modifier: Modifier) {
+    Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black),
+        onClick = { onCardClicked() },
+        modifier = modifier
+            .padding(bottom = dimensionResource(id = R.dimen.vertical_padding))
+            .padding(horizontal = dimensionResource(id = R.dimen.horizontal_padding))
+            .height(60.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Text(
+            text = "이 카드 뽑기", style = TalkbbokkiTypography.button_large, color = Color.White
+        )
+    }
+}
+
+@Composable
+fun CardItem(definiteIndex: Int, currentOffset: Float) {
     val pageOffsetWithSign = definiteIndex - currentOffset
     val pageOffset = pageOffsetWithSign.absoluteValue
-    Box(
-        modifier = Modifier
-            .width(120.dp)
-            .aspectRatio(0.7f)
-            .background(Color.Transparent)
-            .zIndex(5f - pageOffset)
-            .graphicsLayer {
-
-                // 중간으로 올수록 크게 보임
-                lerp(
-                    start = if (pageOffset > 1.5) {
-                        0.8f.dp
-                    } else if (pageOffset > 0.5) {
-                        1.0f.dp
-                    } else {
-                        1.2f.dp
-                    },
-                    stop = if (pageOffset > 1.5) {
-                        1.0f.dp
-                    } else if (pageOffset > 0.5) {
-                        1.2f.dp
-                    } else {
-                        1.9f.dp
-                    },
-                    fraction = 1f - pageOffset.coerceIn(0f, 2f)
-                ).let { scale ->
-                    scaleX = scale.value
-                    scaleY = scale.value
-                }
-
-                // 중간으로 올수록 0도에 가까워짐. (카드 사이 각도 15도씩 틀어짐)
-                lerp(
-                    start = pageOffsetWithSign * 15f.dp, // -30, -15, 0, 15, 30 ...
-                    stop = 0f.dp,
-                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                ).value.let { angle ->
-                    rotationZ = angle
-                }
-
-                // 중간에서 멀어질수록 수직으로 내려가도록 조정.
-                lerp(
-                    start = pageOffset * 50f.dp,
-                    stop = 0f.dp,
-                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                ).value.let { yOffset ->
-                    translationY = yOffset
-                }
-
-                // 카드가 겹치게 보이도록 중앙으로 모이도록 조정.
-                lerp(
-                    start = pageOffsetWithSign * pageOffset * (50f * -1).dp,
-                    stop = 0f.dp,
-                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                ).value.let { xOffset ->
-                    translationX = xOffset
-                }
+    Box(modifier = Modifier
+        .width(dimensionResource(id = R.dimen.card_width))
+        .aspectRatio(0.7f)
+        .background(Color.Transparent)
+        .zIndex(5f - pageOffset)
+        .graphicsLayer {
+            // 중간으로 올수록 크게 보임
+            lerp(
+                start = 1f.dp, stop = 1.9.dp, fraction = 1f - pageOffset.coerceIn(0f, 1.3f)
+            ).let { scale ->
+                scaleX = scale.value
+                scaleY = scale.value
             }
-    ) {
-        val image: Painter = if (pageOffset > 1.5) {
-            painterResource(id = R.drawable.bg_card_small)
-        } else if (pageOffset > 0.5) {
-            painterResource(id = R.drawable.bg_card_regular)
-        } else {
-            painterResource(id = R.drawable.bg_card_large)
-        }
+
+            // 중간으로 올수록 0도에 가까워짐. (카드 사이 각도 15도씩 틀어짐)
+            lerp(
+                start = pageOffsetWithSign * 15f.dp, // -30, -15, 0, 15, 30 ...
+                stop = 0f.dp, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+            ).value.let { angle ->
+                rotationZ = angle
+            }
+
+            // 중간에서 멀어질수록 수직으로 내려가도록 조정.
+            lerp(
+                start = pageOffset * 50f.dp,
+                stop = 0f.dp,
+                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+            ).value.let { yOffset ->
+                translationY = yOffset
+            }
+
+            // 카드가 겹치게 보이도록 중앙으로 모이도록 조정.
+            lerp(
+                start = pageOffsetWithSign * pageOffset * (50f * -1).dp,
+                stop = 0f.dp,
+                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+            ).value.let { xOffset ->
+                translationX = xOffset
+            }
+        }) {
+        val cardImage: Painter =
+            if (pageOffset > 1.5) painterResource(id = R.drawable.bg_card_small)
+            else if (pageOffset > 0.5) painterResource(id = R.drawable.bg_card_regular)
+            else painterResource(id = R.drawable.bg_card_large)
 
         Image(
-            painter = image,
+            painter = cardImage,
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
         )
-        Text(
-            text = definiteIndex.toString()
-        )
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            val category = painterResource(id = R.drawable.ic_tag_love)
+            var isCenter by remember { mutableStateOf(true) }
+
+            isCenter = pageOffset < 0.5
+
+            AnimatedVisibility(
+                visible = isCenter, enter = fadeIn(), exit = fadeOut()
+            ) {
+                Image(
+                    painter = category,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 32.dp)
+                )
+            }
+        }
     }
 }
