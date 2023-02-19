c61c77cc9a2f63f2c340e013da624aac1d882a85
jiyoung <ji9310@naver.com>
2023-02-20 00:46:30 +0900

    메인 화면 : 자체 디자인 QA
    - 스크롤 엣지 효과 없애기

--- app/src/main/java/com/hammer/talkbbokki/ui/theme/Theme.kt
+++ app/src/main/java/com/hammer/talkbbokki/ui/theme/Theme.kt
@@ -1,11 +1,14 @@
 package com.hammer.talkbbokki.ui.theme
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -14,9 +17,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = MainColor01,
+    primaryVariant = MainColor01,
+    secondary = MainColor02
 
     /* Other default colors to override
     background = Color.White,
@@ -28,18 +31,22 @@ private val LightColorPalette = lightColors(
     */
 )
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun IcebreakerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun TalkbbokkiTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-
     MaterialTheme(
         colors = colors,
         typography = Typography(),
-        shapes = Shapes,
-        content = content
-    )
+        shapes = Shapes
+    ) {
+        CompositionLocalProvider(
+            LocalOverscrollConfiguration provides null,
+            content = content
+        )
+    }
 }
