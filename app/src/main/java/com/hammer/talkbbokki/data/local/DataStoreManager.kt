631ef7377742e1ff2933a11289031048df7af160
jiyoung <ji9310@naver.com>
2023-02-19 21:04:53 +0900

    #43 preference 온보딩 로직 추가

--- app/src/main/java/com/hammer/talkbbokki/data/local/DataStoreManager.kt
+++ app/src/main/java/com/hammer/talkbbokki/data/local/DataStoreManager.kt
@@ -1,31 +1,43 @@
 package com.hammer.talkbbokki.data.local
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.hammer.talkbbokki.data.local.PreferenceKeys.SHOW_ON_BOARDING
+import com.hammer.talkbbokki.data.local.PreferenceKeys.VIEW_COUNT
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 private const val STORE_NAME = "talkbbokki"
 private val Context.dataStore by preferencesDataStore(STORE_NAME)
 
-@Singleton
+object PreferenceKeys {
+    val SHOW_ON_BOARDING = booleanPreferencesKey("onBoarding")
+    val VIEW_COUNT = intPreferencesKey("viewCnt")
+}
+
 class DataStoreManager @Inject constructor(@ApplicationContext appContext: Context) {
 
     private val settingsDataStore = appContext.dataStore
-    private val key = intPreferencesKey("viewCnt")
+
+    val viewCnt: Flow<Int> = settingsDataStore.data.map { preferences ->
+        preferences[VIEW_COUNT] ?: 0
+    }
 
     suspend fun setViewCnt(isReset: Boolean = false) {
         settingsDataStore.edit { talkbbokki ->
-            talkbbokki[key] = if (isReset) 0 else (talkbbokki[key] ?: 0) + 1
+            talkbbokki[VIEW_COUNT] = if (isReset) 0 else (talkbbokki[VIEW_COUNT] ?: 0) + 1
         }
     }
 
-    val viewCnt: Flow<Int> = settingsDataStore.data.map { preferences ->
-        preferences[key] ?: 0
+    val showOnBoarding: Flow<Boolean> = settingsDataStore.data.map { it[SHOW_ON_BOARDING] ?: true }
+    suspend fun updateShowOnBoarding() {
+        settingsDataStore.edit { pref ->
+            pref[SHOW_ON_BOARDING] = false
+        }
     }
 }
