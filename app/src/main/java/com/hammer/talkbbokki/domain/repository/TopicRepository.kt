5b3310dfa78b8d06f025e9715b0dd0dc1ac67121
jiyoung <ji9310@naver.com>
2023-02-20 22:05:10 +0900

    #48 토픽 리스트에서 오픈한 카드 id값 저장하도록 로직 수정

--- app/src/main/java/com/hammer/talkbbokki/domain/repository/TopicRepository.kt
+++ app/src/main/java/com/hammer/talkbbokki/domain/repository/TopicRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface TopicRepository {
     fun getTopicList(level: String): Flow<List<TopicItem>>
     fun getTodayViewCnt(): Flow<Int>
-    fun setTodayViewCnt(isReset: Boolean = false): Flow<Int>
+    fun setTodayViewCnt(id: Int): Flow<Int>
 
     fun getOpenedIndex(): Flow<Set<String>>
     fun setOpenedIndex(isReset: Boolean = false, index: String): Flow<Set<String>>
