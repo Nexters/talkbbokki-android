683360e6921f19b46fffb33b28ee504bc5be4a1f
jiyoung <ji9310@naver.com>
2023-03-01 00:13:00 +0900

    filpper 디버깅 툴 연동

--- app/src/main/java/com/hammer/talkbbokki/TalkbbokkiApp.kt
+++ app/src/main/java/com/hammer/talkbbokki/TalkbbokkiApp.kt
@@ -1,6 +1,15 @@
 package com.hammer.talkbbokki
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,5 +18,21 @@ class TalkbbokkiApp : Application() {
     override fun onCreate() {
         super.onCreate()
         MobileAds.initialize(this)
+
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(
+                    InspectorFlipperPlugin(
+                        this@TalkbbokkiApp,
+                        DescriptorMapping.withDefaults()
+                    )
+                )
+                addPlugin(SharedPreferencesFlipperPlugin(this@TalkbbokkiApp, "talkbbokki"))
+                addPlugin(CrashReporterPlugin.getInstance())
+                addPlugin(DatabasesFlipperPlugin(this@TalkbbokkiApp))
+                addPlugin(NetworkFlipperPlugin())
+            }.start()
+        }
     }
 }
